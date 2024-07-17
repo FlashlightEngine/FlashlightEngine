1c619d573ebdc9202a27d2076d989bf6a2289062
Jean <jean.letessier@protonmail.com>
2024-07-17 19:10:04 +0200

    Refactor: Moved logger methods code into Logger.inl.

--- FlashlightEngine/Include/FlashlightEngine/Core/Logger.hpp
+++ FlashlightEngine/Include/FlashlightEngine/Core/Logger.hpp
@@ -40,61 +40,38 @@ public:
     namespace Log {
 
         template<typename... Args>
-        constexpr void EngineTrace(Args&&... args) {
-            Logger::GetEngineLogger()->trace(std::forward<Args>(args)...);
-        }
+        constexpr void EngineTrace(Args&&... args);
 
         template<typename... Args>
-        constexpr void EngineInfo(Args&&... args) {
-            Logger::GetEngineLogger()->info(std::forward<Args>(args)...);
-        }
+        constexpr void EngineInfo(Args&&... args);
 
         template<typename... Args>
-        constexpr void EngineWarn(Args&&... args) {
-            Logger::GetEngineLogger()->warn(std::forward<Args>(args)...);
-        }
+        constexpr void EngineWarn(Args&&... args);
 
         template<typename... Args>
-        constexpr void EngineError(Args&&... args) {
-            Logger::GetEngineLogger()->error(std::forward<Args>(args)...);
-        }
+        constexpr void EngineError(Args&&... args);
 
         template<typename... Args>
-        constexpr void EngineCritical(Args&&... args) {
-            Logger::GetEngineLogger()->critical(std::forward<Args>(args)...);
-        }
+        constexpr void EngineCritical(Args&&... args);
 
 
         template<typename... Args>
-        constexpr void AppTrace(Args&&... args) {
-            Logger::GetApplicationLogger()->trace(std::forward<Args>(args)...);
-        }
+        constexpr void AppTrace(Args&&... args);
 
         template<typename... Args>
-        constexpr void AppInfo(Args&&... args) {
-            Logger::GetApplicationLogger()->info(std::forward<Args>(args)...);
-        }
+        constexpr void AppInfo(Args&&... args);
 
         template<typename... Args>
-        constexpr void AppWarn(Args&&... args) {
-            Logger::GetApplicationLogger()->warn(std::forward<Args>(args)...);
-        }
+        constexpr void AppWarn(Args&&... args);
 
         template<typename... Args>
-        constexpr void AppError(Args&&... args) {
-            Logger::GetApplicationLogger()->error(std::forward<Args>(args)...);
-        }
+        constexpr void AppError(Args&&... args);
 
         template<typename... Args>
-        constexpr void AppCritical(Args&&... args) {
-            Logger::GetApplicationLogger()->critical(std::forward<Args>(args)...);
-        }
+        constexpr void AppCritical(Args&&... args);
 
         template<typename... Args>
-        constexpr void AppFatal(const ErrorCode errorCode, Args&&... args) {
-            Logger::GetApplicationLogger()->critical(std::forward<Args>(args)...);
-            exit(errorCode.GetFormattedErrorCode());
-        }
+        constexpr void AppFatal(ErrorCode errorCode, Args&&... args);
 }
 
 #include "Logger.inl"
--- FlashlightEngine/Include/FlashlightEngine/Core/Logger.inl
+++ FlashlightEngine/Include/FlashlightEngine/Core/Logger.inl
@@ -7,3 +7,64 @@ inline spdlog::logger *Logger::GetEngineLogger() {
 inline spdlog::logger *Logger::GetApplicationLogger() {
     return m_ApplicationLogger.get();
 }
+
+namespace Log {
+
+template<typename... Args>
+constexpr void EngineTrace(Args&&... args) {
+    Logger::GetEngineLogger()->trace(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void EngineInfo(Args&&... args) {
+    Logger::GetEngineLogger()->info(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void EngineWarn(Args&&... args) {
+    Logger::GetEngineLogger()->warn(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void EngineError(Args&&... args) {
+    Logger::GetEngineLogger()->error(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void EngineCritical(Args&&... args) {
+    Logger::GetEngineLogger()->critical(std::forward<Args>(args)...);
+}
+
+
+template<typename... Args>
+constexpr void AppTrace(Args&&... args) {
+    Logger::GetApplicationLogger()->trace(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void AppInfo(Args&&... args) {
+    Logger::GetApplicationLogger()->info(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void AppWarn(Args&&... args) {
+    Logger::GetApplicationLogger()->warn(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void AppError(Args&&... args) {
+    Logger::GetApplicationLogger()->error(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void AppCritical(Args&&... args) {
+    Logger::GetApplicationLogger()->critical(std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+constexpr void AppFatal(const ErrorCode errorCode, Args&&... args) {
+    Logger::GetApplicationLogger()->critical(std::forward<Args>(args)...);
+    exit(errorCode.GetFormattedErrorCode());
+}
+
+}
