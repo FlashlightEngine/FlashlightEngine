66e8babb6621c34addf5d28ffd1b2faf5e3508c7
Jean <jean.letessier@protonmail.com>
2024-07-19 11:58:54 +0200

    Feat : Added glfw3webgpu to dependencies.

--- xmake.lua
+++ xmake.lua
@@ -19,7 +19,7 @@ if (is_mode("debug")) then
     add_defines("FL_DEBUG")
 end
 
-add_requires("glfw 3.4", "glm 1.0.1", "spdlog v1.9.0", "wgpu-native v0.19.4+1", "magic_enum v0.9.5")
+add_requires("glfw 3.4", "glfw3webgpu v1.2.0", "glm 1.0.1", "spdlog v1.9.0", "wgpu-native v0.19.4+1", "magic_enum v0.9.5")
 
 local outputdir = "$(mode)-$(os)-$(arch)"
 
