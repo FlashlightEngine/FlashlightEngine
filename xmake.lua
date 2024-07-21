a21c102228a162b8555b2aadf90ec87d994bde08
Jean <jean.letessier@protonmail.com>
2024-07-21 20:49:43 +0200

    Feat : Added volk (vulkan meta-loader) to dependencies.
    Volk is a Vulkan meta-loader that, primarily allows to load the Vulkan API, but also to load extensions' function pointers automatically.

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-set_xmakever("2.8.8")
+set_xmakever("2.9.3")
 
 set_project("FlashlightEngine")
 set_version("1.0.0")
@@ -19,33 +19,41 @@ if (is_mode("debug")) then
     add_defines("FL_DEBUG")
 end
 
-add_requires("glfw 3.4", "glm 1.0.1", "spdlog v1.9.0", "magic_enum v0.9.5")
+-- Define packages to download.
+add_requires("volk 1.3.283+0", "glfw 3.4", "glm 1.0.1", "spdlog v1.9.0", "magic_enum v0.9.5")
 
 local outputdir = "$(mode)-$(os)-$(arch)"
 
+-- A rule to copy resources from a target's 'Resources' directory.
 rule("cp-resources")
   after_build(function (target)
     os.cp(target:name() .. "/Resources", "build/" .. outputdir .. "/" .. target:name() .. "/bin")
   end)
 
 target("FlashlightEngine")
+  -- Build as a static library
   set_kind("static")
-  
+
+  -- Set binary and object files directories.
   set_targetdir("build/" .. outputdir .. "/FlashlightEngine/bin")
   set_objectdir("build/" .. outputdir .. "/FlashlightEngine/obj")
 
+  -- Set source cpp files.
   add_files("FlashlightEngine/Source/**.cpp")
 
+  -- Add Engine headers to the project and set the include directory as public so it can be accessed from dependant
+  -- targets.
   add_headerfiles("FlashlightEngine/Include/**.hpp", "FlashlightEngine/Include/**.h", "FlashlightEngine/Include/**.inl")
   add_includedirs("FlashlightEngine/Include", {public = true})
 
+  -- Precompiled header
   set_pcxxheader("FlashlightEngine/Include/FlashlightEngine/pch.hpp")
 
-  add_packages("glfw", "glm", "spdlog", {public = true})
+  -- public dependencies
+  add_packages("volk", "glfw", "glm", "spdlog", {public = true})
+  -- private dependencies
   add_packages("magic_enum")
 
-  add_defines("WEBGPU_BACKEND_WGPU")
-  
 target("TestApplication")
     set_kind("binary")
 
@@ -53,7 +61,7 @@ target("TestApplication")
     set_objectdir("build/" .. outputdir .. "/TestApplication/obj")
 
     add_files("TestApplication/Source/**.cpp")
-    add_headerfiles("TestApplication/Shaders/**") -- A little trick to make them show in VS solutions.
+    add_headerfiles("TestApplication/Shaders/**") -- A little trick to make them show in the VS/Rider solution.
     add_headerfiles("TestApplication/Include/**.hpp", "TestApplication/Include/**.h", "TestApplication/Include/**.inl")
     add_includedirs("TestApplication/Include")
 
